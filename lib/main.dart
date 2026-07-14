74af4140d8b238babf82534750a140e256c3a6bd
johnymahmud <johnymahmud@gmail.com>
2026-07-14 18:17:00 +0600

    Initialize Firebase foundation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const RadioCharuApp());
 }
 
